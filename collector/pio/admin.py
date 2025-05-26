70590760f1547c6d5416d369025b3d2947fb3c90
Christopher Ghormley <chris@ghormley.net>
2025-05-25 17:38:54 -0700

    Cleaning up changes from merge.

--- collector/pio/admin.py
+++ collector/pio/admin.py
@@ -1,13 +1,16 @@
 import csv
 from django.contrib import admin
 from django.http import HttpResponse
+from django.contrib.gis.db.models import JSONField
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
-from .models import SurveyPoint, MapConfig, VisitorBehavior, ResponseSummary
+from .models import SurveyPoint, MapConfig, GeoJSONLayer, VisitorBehavior, ResponseSummary
+
+from django_json_widget.widgets import JSONEditorWidget
 
 class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
@@ -21,7 +24,7 @@ class ExportCsvMixin:
 
         writer.writerow(field_names)
         for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
+            writer.writerow([getattr(obj, field) for field in field_names])
 
         return response
 
@@ -29,25 +32,36 @@ class ExportCsvMixin:
 
 @admin.register(SurveyPoint)
 class SurveyPointAdmin(LeafletGeoAdmin, ExportCsvMixin):
-    fields = ['surveyid', 'description', 'responseid', 'projectid', 'ipaddress', 'timestamp', 'timestamp_add', 'timestamp_edit', 'radius', 'resolution', 'geom', 'deleted']
-    readonly_fields = ['surveyid', 'projectid', 'ipaddress', 'timestamp', 'timestamp_add', 'timestamp_edit', 'radius', 'resolution', 'responseid']
+    fields = ['surveyid', 'mapconfig', 'description', 'responseid', 'projectid', 'ipaddress', 'timestamp', 'timestamp_add', 'timestamp_edit', 'radius', 'resolution', 'geom', 'deleted']
+    readonly_fields = ['surveyid', 'projectid', 'ipaddress', 'timestamp', 'timestamp_add', 'timestamp_edit', 'radius', 'resolution', 'responseid', 'mapconfig',]
     list_filter = ['surveyid', 'deleted', 'responseid', 'ipaddress', 'radius', ]
     search_fields = ['surveyid', 'ipaddress', 'radius', 'responseid', 'geom']
     actions = ["export_as_csv"]
 
 @admin.register(MapConfig)
 class MapConfigAdmin(admin.ModelAdmin, ExportCsvMixin):
-    fields = ['name', 'slug', 'config',]
+    fields = ['name', 'slug', 'config', 'layers',]
     list_display = ('name', 'map_link',)
     prepopulated_fields = {"slug": ("name",)}
     readonly_fields = ['map_link',]
     search_fields = ['name', 'slug', 'config',]
     actions = ["export_as_csv"]
 
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditorWidget(options={'mode': 'text'})},
+    }
+
     def map_link(self, obj):
         return mark_safe(f'<a target="_" href="{ reverse("site_index", args=[obj.slug,]) }">{ obj.name }')
     map_link.short_description = 'map link'
 
+@admin.register(GeoJSONLayer)
+class GeoJSONLayerAdmin(admin.ModelAdmin, ExportCsvMixin):
+    fields = ['name', 'slug', 'geojson',]
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ['name', 'slug', 'geojson',]
+    actions = ["export_as_csv"]
+    
 @admin.register(VisitorBehavior)
 class VisitorBehaviorAdmin(admin.ModelAdmin, ExportCsvMixin):
     fields = ['responseid', 'timestamp_add', 'logdata',]
